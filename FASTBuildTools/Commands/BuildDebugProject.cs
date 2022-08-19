9673fb13a34de53b1e45035d97958d3a8f969aa1
Piotr Doan <piotr.doan@cdprojektred.com>
2022-08-19 12:31:58 +0200

    Greatly simplify BuildDebubProject implementation

--- FASTBuildTools/Commands/BuildDebugProject.cs
+++ FASTBuildTools/Commands/BuildDebugProject.cs
@@ -1,43 +1,20 @@
 ﻿using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.VisualStudio.VCProjectEngine;
 using System;
 using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 
 namespace FASTBuildTools
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
     internal sealed class BuildDebugProject
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 4129;
-
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
         public static readonly Guid CommandSet = new Guid("d3d22de7-8b47-49ed-b994-0c1debd5949d");
-
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly AsyncPackage package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BuildDebugProject"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
         private BuildDebugProject(AsyncPackage package, OleMenuCommandService commandService)
         {
             this.package = package ?? throw new ArgumentNullException(nameof(package));
@@ -48,18 +25,12 @@ namespace FASTBuildTools
             commandService.AddCommand(menuItem);
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static BuildDebugProject Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
         private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
         {
             get
@@ -68,44 +39,29 @@ namespace FASTBuildTools
             }
         }
 
-        private DTE2 dte;
-        private IVsSolution vsSolution;
-        private BuildEvents buildEvents;
+        private DTE2 DTE;
+        private IVsSolution VsSolution;
+        private BuildEvents BuildEvents;
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in BuildDebugProject's constructor requires
-            // the UI thread.
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
             OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
             Instance = new BuildDebugProject(package, commandService);
 
-            Instance.dte = Package.GetGlobalService(typeof(_DTE)) as DTE2;
-            Instance.vsSolution = Package.GetGlobalService(typeof(SVsSolution)) as IVsSolution;
+            Instance.DTE = Package.GetGlobalService(typeof(_DTE)) as DTE2;
+            Instance.VsSolution = Package.GetGlobalService(typeof(SVsSolution)) as IVsSolution;
 
-            Instance.buildEvents = Instance.dte.Events.BuildEvents;
-            Instance.dte.Events.BuildEvents.OnBuildBegin += Instance.BuildEvents_OnBuildBegin;
-            Instance.dte.Events.BuildEvents.OnBuildDone += Instance.BuildEvents_OnBuildDone;
-            Instance.dte.Events.BuildEvents.OnBuildProjConfigDone += Instance.BuildEvents_OnBuildProjConfigDone;
+            Instance.BuildEvents = Instance.DTE.Events.BuildEvents;
+            Instance.DTE.Events.BuildEvents.OnBuildBegin += Instance.BuildEvents_OnBuildBegin;
+            Instance.DTE.Events.BuildEvents.OnBuildDone += Instance.BuildEvents_OnBuildDone;
+            Instance.DTE.Events.BuildEvents.OnBuildProjConfigDone += Instance.BuildEvents_OnBuildProjConfigDone;
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
-
-            // TODO: Disable execution and gray out the button while build is running (use OnBuildBegin/OnBuildDone event)?
 
             if (BuildRunning)
                 return;
@@ -127,7 +83,7 @@ namespace FASTBuildTools
 
             if (executeFallback)
             {
-                dte.Solution.SolutionBuild.Debug();
+                DTE.Solution.SolutionBuild.Debug();
             }
         }
 
@@ -135,84 +91,27 @@ namespace FASTBuildTools
         private bool ProjectBuilt = false;
         private bool DebugAfterBuildDone = false;
 
-        private Project FindProject(Solution solution, string searchName)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            foreach (Project project in solution)
-            {
-                var found = FindProject(project, searchName);
-                if (found != null)
-                    return found;
-            }
-
-            return null;
-        }
-
-        private Project FindProject(Project project, string name)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            if (project == null)
-                return null;
-
-            if (project.UniqueName == name)
-                return project;
-
-            if (project.ProjectItems != null)
-            {
-                foreach (ProjectItem item in project.ProjectItems)
-                {
-                    if (item.SubProject != null)
-                    {
-                        var found = FindProject(item.SubProject, name);
-                        if (found != null)
-                            return found;
-                    }
-                }
-            }
-
-            return null;
-        }
-
         private bool HandleFastBuildProject()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var startupProjects = (Array)dte.Solution.SolutionBuild.StartupProjects;
-            if (startupProjects.Length == 0)
+            var solutionBuildManager = Package.GetGlobalService(typeof(SVsSolutionBuildManager)) as IVsSolutionBuildManager2;
+            solutionBuildManager.get_StartupProject(out IVsHierarchy projectHierarchy);
+            if (projectHierarchy == null)
                 return false;
 
-            var activeConfiguration = dte.Solution.SolutionBuild.ActiveConfiguration;
-            foreach (var startupProject in startupProjects)
-            {
-                var project = FindProject(dte.Solution, startupProject.ToString());
-                var vcProject = project?.Object as VCProject;
-                if (vcProject == null)
-                    return false;
+            projectHierarchy.GetProperty(VSConstants.VSITEMID_ROOT, (int)__VSHPROPID.VSHPROPID_ExtObject, out object projectObject); ;
+            var startupProject = projectObject as Project;
+            if (startupProject == null)
+                return false;
 
-                var vcConfiguration = vcProject.ActiveConfiguration;
-                var vcTools = (IVCCollection)vcConfiguration?.Tools;
-                if (vcTools == null)
-                    return false;
+            var activeConfiguration = DTE.Solution.SolutionBuild.ActiveConfiguration as SolutionConfiguration2;
 
-                bool isFastBuild = false;
-                foreach (var vcTool in vcTools)
-                {
-                    var vcMakeTool = vcTool as VCNMakeTool;
-                    if (vcMakeTool != null && vcMakeTool.BuildCommandLine.Contains("FBuild"))
-                    {
-                        isFastBuild = true;
-                        break;
-                    }
-                }
-
-                if (!isFastBuild)
-                    return false;
-
-                DebugAfterBuildDone = true;
-                dte.Solution.SolutionBuild.BuildProject(activeConfiguration.Name, startupProject.ToString(), false);
-            }
+            DebugAfterBuildDone = true;
+            DTE.Solution.SolutionBuild.BuildProject(
+                $"{activeConfiguration.Name}|{activeConfiguration.PlatformName}",
+                startupProject.UniqueName, WaitForBuildToFinish: false
+            );
 
             return true;
         }
@@ -246,7 +145,7 @@ namespace FASTBuildTools
 
             if (DebugAfterBuildDone && ProjectBuilt)
             {
-                dte.Solution.SolutionBuild.Debug();
+                DTE.Solution.SolutionBuild.Debug();
             }
 
             DebugAfterBuildDone = false;
